ca505619929b695c6278e1dfdfb4981473329f0f
CDVCvask <cesarcamposeco415@gmail.com>
2026-03-17 18:29:12 -0600

    Agregar archivos de proyecto.

---
+++ act14 ejercicio1/Program.cs
@@ -0,0 +1,32 @@
+﻿
+class Estudiante
+{
+    public string Nombre;
+    public double Nota1;
+    public double Nota2;
+    public double Nota3;
+
+    public double calcularPromedio()
+    {
+        return (Nota1 + Nota2 + Nota3) / 3;
+    }
+    public string obtenerestado()
+    {
+        if (calcularPromedio() >= 61)
+        {
+            return "Aprobado";
+        }
+        else
+        {
+            return "Reprobado";
+        }
+    }
+
+    public void mostrardatos()
+    {
+        Console.WriteLine($"Nombre: {Nombre}");
+        Console.WriteLine($"Promedio: {calcularPromedio():F2}");
+        Console.WriteLine($"Estado: {obtenerestado()}");
+    }
+}
+
