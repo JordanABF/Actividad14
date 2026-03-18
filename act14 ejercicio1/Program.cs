2b1e1334fc04d8bc911d67d83e4028e298806417
CDVCvask <cesarcamposeco415@gmail.com>
2026-03-17 18:55:08 -0600

    primercaso

--- act14 ejercicio1/Program.cs
+++ act14 ejercicio1/Program.cs
@@ -30,3 +30,33 @@ class Estudiante
     }
 }
 
+List<Estudiante> estudiantes = new List<Estudiante>();
+Console.WriteLine("CUANTOS ESTUDIANTES DESEA INGRESAR");
+int cantidad = int.Parse(Console.ReadLine());
+
+for(int i=0; i<cantidad;i++)
+{
+    Estudiante e=new Estudiante();
+    Console.WriteLine($"INGRESE EL NOMBRE DEL ESTUDIANTE {i+1}: "); e.Nombre=Console.ReadLine();
+    Console.WriteLine("NOTA 1: "); e.Nota1=double.Parse(Console.ReadLine());
+    Console.WriteLine("NOTA 2: "); e.Nota2 = double.Parse(Console.ReadLine());
+    Console.WriteLine("NOTA 3: "); e.Nota3 = double.Parse(Console.ReadLine());
+    estudiantes.Add(e);
+}
+
+double sumaPromedios = 0;
+Estudiante mejorEstudiante = estudiantes[0];
+Console.WriteLine("LISTA DE ESTUDIANTES:");
+foreach(Estudiante e in estudiantes)
+{
+    e.mostrardatos();
+    sumaPromedios += e.calcularPromedio();
+    if(e.calcularPromedio() > mejorEstudiante.calcularPromedio())
+    {
+        mejorEstudiante = e;
+    }
+}
+Console.WriteLine("EL PROMEDIO GENERAL ES: " + (sumaPromedios / estudiantes.Count):F2);
+Console.WriteLine("EL MEJOR ESTUDIANTE ES: ");
+mejorEstudiante.mostrardatos();
+
