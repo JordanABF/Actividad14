8cb7ebaa1ee7a2263d2c028960bb9efaba289736
Jose Boj <Jose Boj@LAPTOP-1O69VUO3>
2026-03-17 23:17:11 -0600

    Caso3.1

---
+++ act14 ejercicio3/Program.cs
@@ -0,0 +1,24 @@
+﻿class Empleado
+{
+    public string Nombre;
+    public string Puesto;
+    public double SalarioMensual;
+    public double CalcularSalarioAnual() { return SalarioMensual * 12; }
+    public double CalcularBono()
+    {
+        if (SalarioMensual >= 5000) return SalarioMensual * 0.15;
+        else return SalarioMensual * 0.08;
+    }
+    public string ClasificarSalario()
+    {
+        if (SalarioMensual >= 7000) return "Salario alto";
+        else if (SalarioMensual >= 4000) return "Salario medio";
+        else return "Salario básico";
+    }
+    public void MostrarDatos()
+    {
+        Console.WriteLine($"Nombre: {Nombre} | Puesto: {Puesto}");
+        Console.WriteLine($"Salario mensual: Q{SalarioMensual:F2} | Salario anual: Q{CalcularSalarioAnual():F2}");
+        Console.WriteLine($" Bono: Q{ CalcularBono():F2} | Nivel: { ClasificarSalario()}");
+    }
+}
