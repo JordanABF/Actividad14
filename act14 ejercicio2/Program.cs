ed608819851b7154521bf99838f98ce2fbc6bcff
CDVCvask <cesarcamposeco415@gmail.com>
2026-03-17 19:10:18 -0600

    CASO2

---
+++ act14 ejercicio2/Program.cs
@@ -0,0 +1,27 @@
+﻿
+class producto
+{
+    public string Nombre;
+    public double Precio;
+    public int Cantidad;
+
+    public double calcularvalorinventario()
+    {
+        return Precio * Cantidad;
+    }
+    public string evaluarstock()
+    {
+        if (Cantidad == 0)
+        {
+            return "SIN EXISTENCIA";
+        }
+        else if (Cantidad <= 5)
+        {
+            return "STOCK BAJO";
+        }
+        else
+        {
+            return "STOCK SUFICIENTE";
+        }
+    }
+}
